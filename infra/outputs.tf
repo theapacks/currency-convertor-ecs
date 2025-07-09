7fa4d7067e728b52cd3c7ea75135e9bb501b3fa8
Mbulaheni Davhana <mbulaheni.d@outlook.com>
2025-07-09 12:55:01 +0200

    feat(infra): add VPC outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,30 @@ output "ecr_repository_url" {
   description = "Name of the ECR repository"
   value       = module.ecr.repository_url
 }
+
+# VPC Outputs
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "intra_subnets" {
+  description = "List of intra subnet IDs"
+  value       = module.vpc.intra_subnets
+  
+}
