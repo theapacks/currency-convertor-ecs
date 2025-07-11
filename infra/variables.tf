02366a27f4fcdcfa50812374e0363f19834fb327
Mbulaheni Davhana <mbulaheni.d@outlook.com>
2025-07-11 23:14:11 +0200

    feat(infra): add ECS configuration variables for ecs

--- infra/variables.tf
+++ infra/variables.tf
@@ -31,3 +31,40 @@ variable "tags" {
     Project = "currency-convertor"
   }
 }
+
+# ECS Configuration
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "currency-convertor-cluster"
+}
+
+variable "ecs_service_name" {
+  description = "Name of the ECS service"
+  type        = string
+  default     = "currency-convertor-service"
+}
+
+variable "ecs_task_cpu" {
+  description = "CPU units for the ECS task"
+  type        = string
+  default     = "256"
+}
+
+variable "ecs_task_memory" {
+  description = "Memory for the ECS task in MiB"
+  type        = string
+  default     = "512"
+}
+
+variable "ecs_desired_count" {
+  description = "Number of ECS task instances to run"
+  type        = number
+  default     = 1
+}
+
+variable "container_port" {
+  description = "Port on which the container listens"
+  type        = number
+  default     = 8080
+}
